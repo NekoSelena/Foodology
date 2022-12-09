e2a46d6ad25bfe52fa20ae3d3b5a1e2952c527a2
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2022-12-09 16:17:06 +0100

    Add request for all recipes without any of the allergens

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ var FOODOLOGY = $rdf.Namespace("http://example.com/owl/foodology#")
 var FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/")
 var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
-const turtleString = fs.readFileSync('data/database.ttl').toString()
+const turtleString = fs.readFileSync('data/users_database.ttl').toString()
 const uri = "http://example.com/owl/foodology"
 
-const store = $rdf.graph()
+const users_store = $rdf.graph()
+const recipes_store = $rdf.graph()
 
 $rdf.parse(
     turtleString,
-    store,
+    users_store,
     uri,
     "text/turtle"
     )
@@ -62,9 +63,9 @@ const stringQuery = `
     }
 `
 
-const query = $rdf.SPARQLToQuery(stringQuery, false, store)
+const query = $rdf.SPARQLToQuery(stringQuery, false, users_store)
 
-var users = store.querySync(query).map(
+var users = users_store.querySync(query).map(
     userResult => {
         return {
             id: userResult['?id'].value,
@@ -99,10 +100,10 @@ app.get("/users", function(request,response){
         let new_user = "http://example.com/owl/foodology#" + fullname.toLowerCase().replace(' ', '_')
         var user = {'id':fullname.toLowerCase().replace(' ', '_'), 'name':fullname}
 
-        // add the new user to the store
-        store.add($rdf.sym(new_user), RDF('type'), FOAF('Person'))
-        store.add($rdf.sym(new_user), FOODOLOGY('id'), fullname.toLowerCase().replace(' ', '_'))
-        store.add($rdf.sym(new_user), FOODOLOGY('name'), fullname)
+        // add the new user to the users_store
+        users_store.add($rdf.sym(new_user), RDF('type'), FOAF('Person'))
+        users_store.add($rdf.sym(new_user), FOODOLOGY('id'), fullname.toLowerCase().replace(' ', '_'))
+        users_store.add($rdf.sym(new_user), FOODOLOGY('name'), fullname)
     }
 
     const model = {
@@ -127,34 +128,34 @@ app.get("/users/:id", function(request, response){
     }
     fullname = arrayname.join(' ')
 
-    if(store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('id'), id, null) == undefined) {
-        store.add($rdf.sym(new_user), RDF('type'), FOAF('Person'))
-        store.add($rdf.sym(new_user), FOODOLOGY('id'), id)
-        store.add($rdf.sym(new_user), FOODOLOGY('name'), fullname)
+    if(users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('id'), id, null) == undefined) {
+        users_store.add($rdf.sym(new_user), RDF('type'), FOAF('Person'))
+        users_store.add($rdf.sym(new_user), FOODOLOGY('id'), id)
+        users_store.add($rdf.sym(new_user), FOODOLOGY('name'), fullname)
     }
 
-    if (Object.keys(request.query).length != 0 && (store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null) != undefined || store.anyStatementMatching($rdf.sym(new_user), null, 'no', null) != undefined)) {
+    if (Object.keys(request.query).length != 0 && (users_store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null) != undefined || users_store.anyStatementMatching($rdf.sym(new_user), null, 'no', null) != undefined)) {
         preferences = request.query
 
-        // add the user's settings in the store
+        // add the user's settings in the users_store
         for (var i = 0; i < Object.keys(preferences).length; i++) {
             var preference = Object.keys(preferences)[i];
-            var remove = store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
+            var remove = users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
             var insert = $rdf.st($rdf.sym(new_user), FOODOLOGY(preference), Object.values(preferences)[i])
-            store.remove(remove)
-            store.add(insert)
+            users_store.remove(remove)
+            users_store.add(insert)
         }
-        let content = $rdf.serialize(undefined, store, null, 'text/turtle')
+        let content = $rdf.serialize(undefined, users_store, null, 'text/turtle')
 
-        // add the user to the database (turtle file)
-        fs.writeFile('data/database.ttl', content, err => {
+        // add the user to the users_database (turtle file)
+        fs.writeFile('data/users_database.ttl', content, err => {
             if (err) {
                 console.error(err);
             }
         });
-    } else if (Object.keys(request.query).length === 0 || (store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null) == undefined && store.anyStatementMatching($rdf.sym(new_user), null, 'no', null) == undefined)) {
-        // console.log(store.anyStatementMatching($rdf.sym(new_user), null, 'no', null))
-        // console.log(store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null))
+    } else if (Object.keys(request.query).length === 0 || (users_store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null) == undefined && users_store.anyStatementMatching($rdf.sym(new_user), null, 'no', null) == undefined)) {
+        // console.log(users_store.anyStatementMatching($rdf.sym(new_user), null, 'no', null))
+        // console.log(users_store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null))
         preferences = {['has_stove']:'no', 
                        ['has_oven']:'no',
                        ['has_pot']:'no',
@@ -169,55 +170,55 @@ app.get("/users/:id", function(request, response){
                        ['allergy_wheat']:'no',
                        ['allergy_soybeans']:'no'}
 
-        // update the user's settings in the store
+        // update the user's settings in the users_store
         for (var i = 0; i < Object.keys(preferences).length; i++) {
             var preference = Object.keys(preferences)[i];
-            var is_value = store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
+            var is_value = users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
             var insert = $rdf.st($rdf.sym(new_user), FOODOLOGY(preference), Object.values(preferences)[i])
             if (is_value == undefined) {
-                store.add(insert)
+                users_store.add(insert)
             } else if (is_value.object.value == 'yes') {
                 preferences[Object.keys(preferences)[i]] = 'yes'
             }
         }
-        let content = $rdf.serialize(undefined, store, null, 'text/turtle')
+        let content = $rdf.serialize(undefined, users_store, null, 'text/turtle')
 
-        // update the user to the database (turtle file)
-        fs.writeFile('data/database.ttl', content, err => {
+        // update the user to the users_database (turtle file)
+        fs.writeFile('data/users_database.ttl', content, err => {
             if (err) {
                 console.error(err);
             }
         });
     } else {
-        preferences = {[store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_stove'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_stove'), null).object.value, 
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_oven'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_oven'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pot'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pot'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pan'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pan'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_kettle'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_kettle'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).object.value, 
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_eggs'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_eggs'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_peanuts'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_peanuts'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_fish'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_fish'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_shellfish'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_shellfish'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_wheat'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_wheat'), null).object.value,
-                       [store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_soybeans'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_soybeans'), null).object.value,
+        preferences = {[users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_stove'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_stove'), null).object.value, 
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_oven'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_oven'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pot'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pot'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pan'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_pan'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_kettle'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('has_kettle'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).object.value, 
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_eggs'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_eggs'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_peanuts'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_peanuts'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_fish'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_fish'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_shellfish'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_shellfish'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_wheat'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_wheat'), null).object.value,
+                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_soybeans'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_soybeans'), null).object.value,
                }
 
-        // update the user's settings in the store
+        // update the user's settings in the users_store
         for (var i = 0; i < Object.keys(preferences).length; i++) {
             var preference = Object.keys(preferences)[i];
-            var remove = store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
+            var remove = users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY(preference), null)
             var insert = $rdf.st($rdf.sym(new_user), FOODOLOGY(preference), Object.values(preferences)[i])
             // console.log("Remove " + remove)
             // console.log("Insert " + insert)
-            store.remove(remove)
-            store.add(insert)
+            users_store.remove(remove)
+            users_store.add(insert)
         }
-        let content = $rdf.serialize(undefined, store, null, 'text/turtle')
+        let content = $rdf.serialize(undefined, users_store, null, 'text/turtle')
 
-        // update the user to the database (turtle file)
-        fs.writeFile('data/database.ttl', content, err => {
+        // update the user to the users_database (turtle file)
+        fs.writeFile('data/users_database.ttl', content, err => {
             if (err) {
                 console.error(err);
             }
@@ -250,12 +251,72 @@ app.get("/recipes", function(request, response){
     response.render("recipes.hbs", model)
 })
 
+
+var recipe = {'name':'NAME',
+              'utensils':'UTENSILS',
+              'ingredients':'INGREDIENTS'}
+
 app.get("/recipes/:id", function(request, response){
 
     const id = request.params.id
-    const recipe = {'name':'NAME',
-                    'utensils':'UTENSILS',
-                    'allergies':'ALLERGIES'}
+    const user = users.find(g => g.id == user_id)
+    var allergy_peanut = "?ingredients a <http://dbpedia.org/resource/Peanut>"
+    var allergy_eggs = "?ingredients a <http://dbpedia.org/resource/Egg_as_food>"
+    var allergy_treenut = "?ingredients a <http://dbpedia.org/resource/Tree_nut>"
+    var allergy_fish = "?ingredients a <http://dbpedia.org/resource/Fish>"
+    var allergy_shellfish = "?ingredients a <http://dbpedia.org/resource/Shellfish>"
+    var allergy_milk = "?ingredients a <http://dbpedia.org/resource/Milk>"
+    var allergy_soybeans = "?ingredients a <http://dbpedia.org/resource/Soybean>"
+    var allergy_wheat = "?ingredients a <http://dbpedia.org/resource/Wheat>"
+
+    const ParsingClient = require('sparql-http-client/ParsingClient')
+
+    const client = new ParsingClient({
+        endpointUrl: 'https://dbpedia.org/sparql'
+    })
+
+    const query = `
+    SELECT DISTINCT ?dishName ?dish
+WHERE
+{
+    ?dish dbo:ingredient ?ingredients .
+    ?dish <http://www.w3.org/2000/01/rdf-schema#label> ?dishName .
+    FILTER (lang(?dishName) = "en") .
+    MINUS {
+        ?ingredients a <http://dbpedia.org/resource/Peanut> .
+        ?ingredients a <http://dbpedia.org/resource/Egg_as_food> .
+        ?ingredients a <http://dbpedia.org/resource/Tree_nut> .
+        ?ingredients a <http://dbpedia.org/resource/Fish> .
+        ?ingredients a <http://dbpedia.org/resource/Shellfish> .
+        ?ingredients a <http://dbpedia.org/resource/Milk> .
+        ?ingredients a <http://dbpedia.org/resource/Soybean> .
+        ?ingredients a <http://dbpedia.org/resource/Wheat> .
+    }
+}
+`
+    client.query.select(query).then(rows => {
+
+        // console.log(rows)
+
+        rows.forEach(row => {
+            //request all the ingredients from FoodData Central + users_store them into array
+            recipe.name = row.dishName.value.replace('@en', '')
+            recipes_store.add($rdf.sym(row.dish.value), RDF('type'), FOODOLOGY('Recipe'))
+            recipes_store.add($rdf.sym(row.dish.value), FOODOLOGY('name'), recipe.name)
+        })
+        
+    }).catch(error => {
+        console.log(error)
+    })
+
+    let content = $rdf.serialize(undefined, recipes_store, null, 'text/turtle')
+
+    // add the recipes to the recipes_database (turtle file)
+    fs.writeFile('data/recipes_database.ttl', content, err => {
+        if (err) {
+            console.error(err);
+        }
+    });
 
     const model = {
         personalizedRecipes: recipe
