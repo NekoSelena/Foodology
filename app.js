e8891286ede1c67464a9715150c7d916bba622d8
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2022-12-11 03:08:29 +0100

    Add working continental preferences

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const turtleString = fs.readFileSync('data/users_database.ttl').toString()
 const uri = "http://example.com/owl/foodology"
 
 const users_store = $rdf.graph()
-const recipes_store = $rdf.graph()
 
 $rdf.parse(
     turtleString,
@@ -31,13 +30,12 @@ const stringQuery = `
     SELECT
         ?id
         ?name
-        ?asian
-        ?north_american
-        ?south_american
-        ?european
-        ?african
-        ?eurasian
-        ?oceanic
+        ?likes_asian
+        ?likes_north_american
+        ?likes_south_american
+        ?likes_european
+        ?likes_african
+        ?likes_oceanic
         ?milk
         ?treenuts
         ?eggs
@@ -50,13 +48,12 @@ const stringQuery = `
         ?user a <http://xmlns.com/foaf/0.1/Person> .
         ?user <http://example.com/owl/foodology#id> ?id .
         ?user <http://example.com/owl/foodology#name> ?name .
-        ?user <http://example.com/owl/foodology#likes_asian> ?asian .
-        ?user <http://example.com/owl/foodology#likes_north_american> ?north_american .
-        ?user <http://example.com/owl/foodology#likes_south_american> ?south_american .
-        ?user <http://example.com/owl/foodology#likes_european> ?european .
-        ?user <http://example.com/owl/foodology#likes_african> ?african .
-        ?user <http://example.com/owl/foodology#likes_eurasian> ?eurasian .
-        ?user <http://example.com/owl/foodology#likes_oceanic> ?oceanic .
+        ?user <http://example.com/owl/foodology#likes_asian> ?likes_asian .
+        ?user <http://example.com/owl/foodology#likes_north_american> ?likes_north_american .
+        ?user <http://example.com/owl/foodology#likes_south_american> ?likes_south_american .
+        ?user <http://example.com/owl/foodology#likes_european> ?likes_european .
+        ?user <http://example.com/owl/foodology#likes_african> ?likes_african .
+        ?user <http://example.com/owl/foodology#likes_oceanic> ?likes_oceanic .
         ?user <http://example.com/owl/foodology#allergy_milk> ?milk .
         ?user <http://example.com/owl/foodology#allergy_treenuts> ?treenuts .
         ?user <http://example.com/owl/foodology#allergy_eggs> ?eggs .
@@ -75,13 +72,12 @@ var users = users_store.querySync(query).map(
         return {
             id: userResult['?id'].value,
             name: userResult['?name'].value,
-            asian: userResult['?asian'].value,
-            north_american: userResult['?north_american'].value,
-            south_american: userResult['?south_american'].value,
-            european: userResult['?european'].value,
-            african: userResult['?african'].value,
-            eurasian: userResult['?eurasian'].value,
-            oceanic: userResult['?oceanic'].value,
+            likes_asian: userResult['?likes_asian'].value,
+            likes_north_american: userResult['?likes_north_american'].value,
+            likes_south_american: userResult['?likes_south_american'].value,
+            likes_european: userResult['?likes_european'].value,
+            likes_african: userResult['?likes_african'].value,
+            likes_oceanic: userResult['?likes_oceanic'].value,
             milk: userResult['?milk'].value,
             treenuts: userResult['?treenuts'].value,
             eggs: userResult['?eggs'].value,
@@ -161,14 +157,11 @@ app.get("/users/:id", function(request, response){
             }
         });
     } else if (Object.keys(request.query).length === 0 || (users_store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null) == undefined && users_store.anyStatementMatching($rdf.sym(new_user), null, 'no', null) == undefined)) {
-        // console.log(users_store.anyStatementMatching($rdf.sym(new_user), null, 'no', null))
-        // console.log(users_store.anyStatementMatching($rdf.sym(new_user), null, 'yes', null))
         preferences = {['likes_asian']:'no',
                        ['likes_north_american']:'no',
                        ['likes_south_american']:'no',
                        ['likes_european']:'no',
                        ['likes_african']:'no',
-                       ['likes_eurasian']:'no',
                        ['likes_oceanic']:'no',
                        ['allergy_milk']:'no',
                        ['allergy_treenuts']:'no',
@@ -204,7 +197,6 @@ app.get("/users/:id", function(request, response){
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_south_american'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_south_american'), null).object.value,
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_european'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_european'), null).object.value,
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_african'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_african'), null).object.value,
-                       [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_eurasian'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_eurasian'), null).object.value,
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_oceanic'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('likes_oceanic'), null).object.value,
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_milk'), null).object.value, 
                        [users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).predicate.value.replace("http://example.com/owl/foodology#", '')]:users_store.anyStatementMatching($rdf.sym(new_user), FOODOLOGY('allergy_treenuts'), null).object.value,
@@ -253,7 +245,7 @@ app.get("/users/:id", function(request, response){
 app.get("/recipes", function(request, response){
 
     const user = users.find(g => g.id == user_id)
-    console.log(user)
+    // console.log(user)
 
     const model = {
         logged_user: user
@@ -262,15 +254,67 @@ app.get("/recipes", function(request, response){
     response.render("recipes.hbs", model)
 })
 
+const recipes_store = $rdf.graph()
+
 app.get("/recipes/:id", function(request, response){
 
     const id = request.params.id
-    const user = users.find(g => g.id == user_id)
+    const user = users.find(g => g.id == id)
+    let filename = 'data/recipes_database_' + id + '.ttl'
     var recipes = []
-    let asian_country = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Asia>"
-    let european_country = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Europe>"
-    let north_american_country = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_North_America>"
-    var regional_preferences = "UNION"
+    let likes_asian = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Asia>"
+    let likes_european = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Europe>"
+    let likes_north_american = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_North_America>"
+    let likes_south_american = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_South_America>"
+    let likes_african = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Africa>"
+    let likes_oceanic = "?dishCountry <http://purl.org/dc/terms/subject> <http://dbpedia.org/resource/Category:Countries_in_Oceania>"
+    var regional_preferences_filter = ""
+    let regional_preferences_user = []
+
+    fs.writeFile(filename, '', err => {
+        if (err) {
+            console.error(err);
+        }
+    });
+
+    // console.log(user)
+
+    for (const [key, value] of Object.entries(user)) {
+        let preference = users_store.match(null, FOODOLOGY(key), 'yes').map(st => key)
+        // console.log(preference)
+        if (preference.length != 0) {
+            switch (key) {
+                case "likes_asian":
+                    regional_preferences_user.push(likes_asian)
+                    break;
+                case "likes_african":
+                    regional_preferences_user.push(likes_african)
+                    break;
+                case "likes_european":
+                    regional_preferences_user.push(likes_european)
+                    break;
+                case "likes_north_american":
+                    regional_preferences_user.push(likes_north_american)
+                    break;
+                case "likes_south_american":
+                    regional_preferences_user.push(likes_south_american)
+                    break;
+                case "likes_oceanic":
+                    regional_preferences_user.push(likes_oceanic)
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    // console.log(regional_preferences_user)
+    let buff = "{ " + regional_preferences_user.join(" } UNION { ") + " }"
+
+    if (buff != "{  }") {
+        //console.log(buff)
+        regional_preferences_filter = buff
+    }
+    // console.log(regional_preferences_filter)
 
     const ParsingClient = require('sparql-http-client/ParsingClient')
 
@@ -287,12 +331,19 @@ app.get("/recipes/:id", function(request, response){
         ?dish <http://dbpedia.org/ontology/country> ?dishCountry .
         ?dish <http://www.w3.org/2000/01/rdf-schema#label> ?dishName .
         FILTER (lang(?dishName) = "en") .
-        ${regional_preferences}
-    }
+        ${regional_preferences_filter}
+    } LIMIT 200
 `
-    client.query.select(query).then(rows => {
 
+    client.query.select(query).then(rows => {
         // console.log(rows)
+
+        if (recipes_store.anyStatementMatching(null, RDF('type'), FOODOLOGY('Recipe')) != undefined) {
+            while (recipes_store.statementsMatching(null, RDF('type'), FOODOLOGY('Recipe')).length != 0) {
+                recipes_store.remove(recipes_store.anyStatementMatching(null, RDF('type'), FOODOLOGY('Recipe')))
+                console.log(recipes_store.statementsMatching(null, RDF('type'), FOODOLOGY('Recipe')).length)
+            }
+        }
 
         rows.forEach(row => {
             recipes_store.add($rdf.sym(row.dish.value), RDF('type'), FOODOLOGY('Recipe'))
@@ -309,6 +360,9 @@ app.get("/recipes/:id", function(request, response){
 
     if (!recipes_store.sameTerm(empty)) {
         var recipes_resources = recipes_store.match(null, RDF('type'), FOODOLOGY('Recipe')).map(st => st.subject.value)
+        while (recipes.pop() != undefined) {
+            recipes.pop()
+        }
         recipes_resources.forEach (resource => {
             let recipe = {}
             recipe.resource = resource
@@ -323,7 +377,6 @@ app.get("/recipes/:id", function(request, response){
     }
 
     let content = $rdf.serialize(undefined, recipes_store, null, 'text/turtle')
-    let filename = 'data/recipes_database_' + id + '.ttl'
 
     // add the recipes to the recipes_database (turtle file)
     fs.writeFile(filename, content, err => {
@@ -342,13 +395,5 @@ app.get("/recipes/:id", function(request, response){
 app.get("/layout.css", function(_request, response){
     response.sendFile("layout.css", {root: "."})
 })
-
-// app.get("/api", function (request, response) {
-
-//     const id = request.query.id
-//     const user = users.find(g => g.id == id)
-
-//     response.send(user)
-// })
 
 app.listen(8080)
